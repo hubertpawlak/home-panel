b227f843d8307bc3686b4012f537592ef8192380
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2022-04-23 18:04:20 +0200

    Restrict trpc "test" prefix to dev env

--- server/routers/test.ts
+++ server/routers/test.ts
@@ -1,10 +1,19 @@
+import { TRPCError } from "@trpc/server";
+import { env } from "process";
 import { z } from "zod";
 import { createRouter } from "../createRouter";
 
-export const testRouter = createRouter().query("timestamp", {
-  input: z.undefined(),
-  output: z.number(),
-  resolve({}) {
-    return Date.now();
-  },
-});
+export const testRouter = createRouter()
+  .middleware(async ({ next }) => {
+    if (env.NODE_ENV !== "development") {
+      throw new TRPCError({ code: "FORBIDDEN" });
+    }
+    return next();
+  })
+  .query("timestamp", {
+    input: z.undefined(),
+    output: z.number(),
+    resolve({}) {
+      return Date.now();
+    },
+  });

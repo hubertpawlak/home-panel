cf5aab51b92f31223740d4207e9e0818eebaeac2
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2022-09-13 21:54:15 +0200

    Allow deleting and renaming sensors

--- server/routers/admin/sensors.ts
+++ server/routers/admin/sensors.ts
@@ -1,3 +1,44 @@
+import supabase from "../../../utils/supabase";
 import { createProtectedRouter } from "../../createProtectedRouter";
+import { definitions } from "../../../types/supabase";
+import { SharedMax } from "../../../types/SharedMax";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
-export const sensorsRouter = createProtectedRouter();
+export const sensorsRouter = createProtectedRouter()
+  .mutation("renameTemperatureSensor", {
+    input: z.object({
+      hwId: z.string().min(1).max(SharedMax),
+      name: z.string().min(1).max(SharedMax),
+    }),
+    async resolve({ input }) {
+      const { hwId, name } = input;
+      const { data, error, status, statusText } = await supabase
+        .from<definitions["temperature_sensors"]>("temperature_sensors")
+        .update({ hwId, name });
+      if (error)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `${status}: ${statusText}`,
+        });
+      return { data };
+    },
+  })
+  .mutation("deleteTemperatureSensor", {
+    input: z.object({
+      hwId: z.string().min(1).max(SharedMax),
+    }),
+    async resolve({ input }) {
+      const { hwId } = input;
+      const { error, status, statusText } = await supabase
+        .from<definitions["temperature_sensors"]>("temperature_sensors")
+        .delete()
+        .match({ hwId });
+      if (error)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `${status}: ${statusText}`,
+        });
+      return { success: true };
+    },
+  });

00019884816801748606565367cb8d9053ca93db
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2022-09-17 16:37:35 +0200

    Bulk temperature insertion

--- server/routers/m2m/temperature.ts
+++ server/routers/m2m/temperature.ts
@@ -6,25 +6,25 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 export const temperatureRouter = createM2MRouter().mutation(
-  "storeTemperature",
+  "storeTemperatures",
   {
-    input: z.object({
-      hwId: z.string().min(1).max(SharedMax),
-      temperature: z.number(),
-      resolution: z.number(),
-    }),
+    input: z
+      .array(
+        z.object({
+          hwId: z.string().min(1).max(SharedMax),
+          temperature: z.number(),
+          resolution: z.number(),
+        })
+      )
+      .min(1),
     async resolve({ ctx, input }) {
       const { sourceId } = ctx;
-      const { hwId, resolution, temperature } = input;
+      // Add sourceId to every reading
+      const _input = input.map((i) => ({ ...i, updated_by: sourceId }));
       const { data, error, status, statusText } = await supabase
         .from<definitions["temperature_sensors"]>("temperature_sensors")
-        .upsert({
-          hwId,
-          temperature,
-          resolution,
-          // updated_at is set by "handle_updated_at" DB trigger
-          updated_by: sourceId,
-        });
+        // updated_at is set by "handle_updated_at" DB trigger
+        .upsert(_input);
       if (error)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",

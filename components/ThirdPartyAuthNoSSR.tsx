62962c3b19a869f603345010d500346a74959bc7
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2022-09-22 14:41:42 +0200

    Removed unused component

--- components/ThirdPartyAuthNoSSR.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
-
-export const ThirdPartyAuthNoSSR = dynamic(
-  new Promise<typeof ThirdParty.ThirdPartyAuth>((res) =>
-    res(ThirdParty.ThirdPartyAuth)
-  ),
-  { ssr: false }
-);

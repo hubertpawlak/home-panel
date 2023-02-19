d0928510a730047490a13b52f862eef9de4e8b39
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2023-02-19 16:31:03 +0100

    fix: Fixed import
    Shouldn't import the named export from default-exporting package.json

--- server/routers/health.ts
+++ server/routers/health.ts
@@ -1,8 +1,8 @@
 // Licensed under the Open Software License version 3.0
-import { version } from "../../package.json";
+import pkg from "../../package.json";
 import { userProcedure } from "../middleware/enforceUserAuth";
 import { router } from "./trpc";
 
 export const appHealthRouter = router({
-  version: userProcedure.query(async () => version),
+  version: userProcedure.query(async () => pkg.version),
 });

c5ca24499025b8d7372b1e18a5e68d6e205b1463
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2023-05-05 19:38:20 +0200

    fix: SuperTokens changed UI route handling

--- pages/auth/[[...path]].tsx
+++ pages/auth/[[...path]].tsx
@@ -2,18 +2,20 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useEffect } from "react";
-import SuperTokens, { redirectToAuth } from "supertokens-auth-react";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import { canHandleRoute, getRoutingComponent } from "supertokens-auth-react/ui";
 
-const SuperTokensComponentNoSSR = dynamic(
-  new Promise((res) => res(SuperTokens.getRoutingComponent)) as any,
+const SuperTokensComponentNoSSR = dynamic<unknown>(
+  new Promise((res) => res(() => getRoutingComponent([ThirdPartyPreBuiltUI]))),
   { ssr: false }
 );
 
 export default function Auth() {
-  // If the user visits a page that is not handled by us (like /auth/random), then we redirect them back to the auth page.
+  // If the user visits a page that is not handled by us (like /auth/random), then we redirect them
   useEffect(() => {
-    if (SuperTokens.canHandleRoute() === false) {
-      redirectToAuth();
+    if (canHandleRoute([ThirdPartyPreBuiltUI]) === false) {
+      // Redirect to home page to avoid infinite redirects
+      window.location.href = "/";
     }
   }, []);
 
